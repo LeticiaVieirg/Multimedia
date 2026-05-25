41ce4bf3fd44b389b8f3ed28f4b9e86141790204
LeticiaVierg <letvng23@gmail.com>
2026-05-25 14:44:16 -0300

    featt: add functions in files

---
+++ profile3/profile/app/data/Musics.ts
@@ -0,0 +1,25 @@
+import Music from "./Music"
+
+const musics: Music[]=
+[
+    {
+        nome:"Bang",
+        url: "audios/Bang.mp3", 
+        imagem: "images/Bang.png",
+        artistas: "Anitta"
+    }, 
+    {
+        nome:"Meiga e Abusada",
+        url: "audios/Meiga.mp3", 
+        imagem: "images/Nova.png",
+        artistas: "Anitta"
+    }, 
+    {
+        nome:"Show das Poderosas",
+        url: "audios/Show.mp3", 
+        imagem: "images/Show.png",
+        artistas: "Anitta"
+    }
+]
+
+export default musics;
